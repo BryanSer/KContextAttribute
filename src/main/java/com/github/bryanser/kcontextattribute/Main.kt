2f16249855351432d0fc8ddef221b46b1251e606
BryanSer <bryan_lzh@njust.edu.cn>
2020-03-04 13:36:42 +0800

    init

---
+++ src/main/java/com/github/bryanser/kcontextattribute/Main.kt
@@ -0,0 +1,11 @@
+package com.github.bryanser.kcontextattribute
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onEnable() { // Plugin startup logic
+    }
+
+    override fun onDisable() { // Plugin shutdown logic
+    }
+}
